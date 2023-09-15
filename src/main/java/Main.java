c0b0f0e87adb5ca9aaff8b815793a192648e8d43
Konrad Putelbergier <Konrad.Putelbergier@gmail.com>
2023-09-15 08:29:52 +0300

    Zadanie 13.2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,6 +12,45 @@ public class Main {
     }
 
     public void run(Scanner scanner) {
-        // napisz swój program tutaj. Do wczytywania danych użyj przekazanego w parametrze scannera
+        List<Integer> listOfNumbers = new ArrayList<>();
+        StringBuilder stringbuilder = new StringBuilder();
+        System.out.println("Podaj liczbę dodatnią");
+        int number = scanner.nextInt();
+        if (number < 0) {
+            System.out.println("Koniec");
+        } else {
+            listOfNumbers.add(number);
+        }
+        while (number >= 0) {
+            System.out.println("Podaj kolejną liczbę dodatnią");
+            number = scanner.nextInt();
+            if (number >= 0) {
+                listOfNumbers.add(number);
+            }
+        }
+        int sum = 0;
+        if (!listOfNumbers.isEmpty()) {
+            int size = listOfNumbers.size() - 1;
+            for (int i = 0; i <= size; i++) {
+                sum += listOfNumbers.get(i);
+
+                if (i != size) {
+                    System.out.print(listOfNumbers.get(size - i) + ", ");
+                    stringbuilder.append(listOfNumbers.get(i) + " + ");
+                } else {
+                    System.out.print(listOfNumbers.get(size - i));
+                    stringbuilder.append(listOfNumbers.get(i));
+                }
+            }
+            List<Integer> sortedList = new ArrayList<>(listOfNumbers);
+            Collections.sort(sortedList);
+            System.out.println();
+            stringbuilder.append(" = " + sum);
+            System.out.println(stringbuilder.toString());
+            System.out.println("Najmniejsza liczba w liście to " + sortedList.get(0));
+            System.out.println("Największa liczba w liście to " + sortedList.get(sortedList.size() - 1));
+        } else {
+            System.out.println("Lista jest pusta");
+        }
     }
 }
