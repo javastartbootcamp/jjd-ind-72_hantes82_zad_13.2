1e9d7475bcfde22388d780bc5fe7fd30f00dada5
Konrad Putelbergier <Konrad.Putelbergier@gmail.com>
2023-10-01 14:55:31 +0200

    Zadanie 13.2 - poprawione

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,45 +12,65 @@ public class Main {
     }
 
     public void run(Scanner scanner) {
+        List<Integer> listOfNumbers = getIntegers(scanner);
+        printNumbers(listOfNumbers);
+        printSum(listOfNumbers);
+    }
+
+    private static List<Integer> getIntegers(Scanner scanner) {
         List<Integer> listOfNumbers = new ArrayList<>();
-        StringBuilder stringbuilder = new StringBuilder();
-        System.out.println("Podaj liczbę dodatnią");
-        int number = scanner.nextInt();
-        if (number < 0) {
-            System.out.println("Koniec");
-        } else {
-            listOfNumbers.add(number);
-        }
-        while (number >= 0) {
-            System.out.println("Podaj kolejną liczbę dodatnią");
+        int number;
+        do {
+            System.out.println("Podaj liczbę dodatnią");
             number = scanner.nextInt();
             if (number >= 0) {
                 listOfNumbers.add(number);
             }
-        }
-        int sum = 0;
-        if (!listOfNumbers.isEmpty()) {
-            int size = listOfNumbers.size() - 1;
-            for (int i = 0; i <= size; i++) {
-                sum += listOfNumbers.get(i);
+        } while (number >= 0);
+        return listOfNumbers;
 
-                if (i != size) {
-                    System.out.print(listOfNumbers.get(size - i) + ", ");
-                    stringbuilder.append(listOfNumbers.get(i) + " + ");
-                } else {
-                    System.out.print(listOfNumbers.get(size - i));
-                    stringbuilder.append(listOfNumbers.get(i));
-                }
-            }
-            List<Integer> sortedList = new ArrayList<>(listOfNumbers);
-            Collections.sort(sortedList);
-            System.out.println();
-            stringbuilder.append(" = " + sum);
-            System.out.println(stringbuilder.toString());
-            System.out.println("Najmniejsza liczba w liście to " + sortedList.get(0));
-            System.out.println("Największa liczba w liście to " + sortedList.get(sortedList.size() - 1));
-        } else {
+    }
+
+    private static void printNumbers(List<Integer> listOfNumbers) {
+
+        if (listOfNumbers.isEmpty()) {
             System.out.println("Lista jest pusta");
+            return;
+        }
+        Integer max = Collections.max(listOfNumbers);
+        Integer min = Collections.min(listOfNumbers);
+        System.out.println("Najmniejsza liczba w liście to " + min);
+        System.out.println("Największa liczba w liście to " + max);
+        reverseNumbers(listOfNumbers);
+    }
+
+    private static void printSum(List<Integer> listOfNumbers) {
+        int sum = 0;
+        StringBuilder stringbuilder = new StringBuilder();
+        int size = listOfNumbers.size() - 1;
+        for (int i = 0; i <= size; i++) {
+            sum += listOfNumbers.get(i);
+            if (i != size) {
+                stringbuilder.append(listOfNumbers.get(i) + " + ");
+            } else {
+                stringbuilder.append(listOfNumbers.get(i));
+            }
+        }
+        stringbuilder.append(" = " + sum);
+        System.out.println(stringbuilder.toString());
+    }
+
+    private static void reverseNumbers(List<Integer> listOfNumbers) {
+        Collections.reverse(listOfNumbers);
+        System.out.println(listOfNumbers);
+        for (int i = 0; i < listOfNumbers.size(); i++) {
+            if (listOfNumbers.get(i) < listOfNumbers.size()) {
+                System.out.print(listOfNumbers.get(i) + ", ");
+            } else {
+                System.out.print(listOfNumbers.get(i));
+                System.out.println();
+            }
         }
     }
 }
+
